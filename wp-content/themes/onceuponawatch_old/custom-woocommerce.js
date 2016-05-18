76b400ede76808d3d760673212cd6293a1de4788
alexandre <akong@ws-kevin-imac.local>
2016-05-18 19:55:30 +0200

    Hello

--- wp-content/themes/onceuponawatch_old/custom-woocommerce.js
+++ wp-content/themes/onceuponawatch_old/custom-woocommerce.js
@@ -21,8 +21,17 @@ $('.shopping li.product').on('mouseleave', function(){
             $('.widget_product_categories, .widget_layered_nav').addClass('inlinefiltres');
             $('.widget_price_filter').css({maxWidth:'1200px', margin:'auto auto 50px auto', listStyleType:'none', textAlign:'center'});
             $('.ui-widget-header').css('background-color', '#d7eefa');
-            $('.ui-slider-range').css('background-color', 'blue')
+            $('.ui-slider-range').css('background-color', 'blue');
 
+            if ($('.product_title').length ) {
+                $('.inboutique').after($('h1'));
+                $('h1').addClass('custom-title-produit');
+                $('.tabs li').addClass('custom-li').removeClass('additional_information_tab description_tab reviews_tab');
+                $('h2').addClass('custom-h2');
+                $('img.wp-post-image').after($('h1'))
+            }
+
+            // Custom button $('.price_slider_amount .button').addClass('custom-button-reverse').removeClass('button');
             //$('.inboutique').after($('.widget_price_filter'));
             //$('.widget_product_categories').before('<div class="inlinefiltres">');
             //$('.widget_layered_nav').after('</div>');
